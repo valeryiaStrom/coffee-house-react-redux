60f4e0482cc846ce06eb0455a2f67e0d5ae2d3d9
Valeryia Strom <valeryia_strom@epam.com>
2024-03-27 19:07:09 +0300

    add redux thunk middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./root-reducer";
 import logger from "redux-logger";
+import { thunk } from "redux-thunk";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export default store;
